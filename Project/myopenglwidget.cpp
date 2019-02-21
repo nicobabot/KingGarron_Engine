6a1f632e9950ccc3d1c1bd23e5d34cd066876059
nicobabot <nico.babotaltisen@gmail.com>
2019-02-21 11:57:53 +0100

    Deleting code from opengl window

--- Project/myopenglwidget.cpp
+++ Project/myopenglwidget.cpp
@@ -25,38 +25,4 @@ void MyOpenGLWidget::paintGL()
 
     glClearColor(1.0f, 0.0f, 0.0f,1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    //GPrimitive test = new GPrimitive();
-
-
-   /* QColor blueColor = QColor::fromRgb(127,190,220);
-    QColor whiteColor = QColor::fromRgb(255,255,255);
-    QColor blackColor = QColor::fromRgb(0,0,0);
-
-    QPainter painter(this);
-    QBrush brush;
-    QPen pen;
-
-    brush.setColor(blueColor);
-    brush.setStyle(Qt::BrushStyle::SolidPattern);
-    pen.setStyle(Qt::PenStyle::NoPen);
-    painter.setBrush(brush);
-    painter.setPen(pen);
-
-    painter.drawRect(rect());
-
-    brush.setColor(whiteColor);
-    pen.setWidth(4);
-    pen.setColor(blackColor);
-    pen.setStyle(Qt::PenStyle::DashLine);
-    painter.setBrush(brush);
-    painter.setPen(pen);
-
-    int r = 64;
-    int w = r*2;
-    int h = r*2;
-    int x = rect().width() / 2 - r;
-    int y = rect().height() / 2 - r;
-    QRect circleRect(x,y,w,h);
-    painter.drawEllipse(circleRect);*/
 }
