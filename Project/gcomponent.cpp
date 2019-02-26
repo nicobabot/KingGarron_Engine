defaf2e83e25d1d5cb98244fbf1a415969f59160
nicobabot <nico.babotaltisen@gmail.com>
2019-02-26 11:34:48 +0100

    Adding methods to let te project compile

--- Project/gcomponent.cpp
+++ Project/gcomponent.cpp
@@ -1,6 +1,20 @@
 #include "gcomponent.h"
+#include <QPainter>
 
 gComponent::gComponent(QWidget *parent) : QWidget(parent)
+{
+    setAutoFillBackground(true);
+}
+
+QSize gComponent::sizeHint() const{
+    return QSize(256,256);
+}
+
+QSize gComponent::minimumSizeHint() const{
+    return QSize(64,64);
+}
+
+void gComponent::paintEvent(QPaintEvent *)
 {
 
 }
