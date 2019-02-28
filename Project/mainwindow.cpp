4fc9f7ffef859fc46896a8002943c144eabb1d51
xavierolivenza <yotolivenza@gmail.com>
2019-02-28 11:31:30 +0100

    Hierarchy creates and destroy things

--- Project/mainwindow.cpp
+++ Project/mainwindow.cpp
@@ -7,9 +7,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->HierarchyAdd, SIGNAL(clicked()), this, SLOT(HierarchyAdd()));
+    connect(ui->HierarchyRemove, SIGNAL(clicked()), this, SLOT(HierarchyRemove()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::HierarchyAdd()
+{
+    std::string name = "Object_";
+    name += std::to_string(objectNum++);
+    ui->HierarchyList->addItem(QString(name.c_str()));
+    //TODO: Also create the gObject...
+}
+
+void MainWindow::HierarchyRemove()
+{
+    qDeleteAll(ui->HierarchyList->selectedItems());
+    //TODO: Also destroy the gObject...
 }
--- Project/mainwindow.h
+++ Project/mainwindow.h
@@ -17,9 +17,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void HierarchyAdd();
+    void HierarchyRemove();
+
 private:
     Ui::MainWindow* ui = nullptr;
     QVector<gObject*> ScenegObjectVector;
+    int objectNum = 0;
 };
 
 #endif // MAINWINDOW_H
